410652d84d6b1f0056c03823bc41abb4102018ab
Dykann Khaov <dykann.khaov@gmail.com>
2021-06-12 02:54:40 +0200

    fix height of diagnostic section

--- src/Diagnostic.js
+++ src/Diagnostic.js
@@ -2,7 +2,7 @@ import Section from './Section'
 
 function Diagnostic() {
   return (
-    <Section className="lg:flex lg:justify-evenly lg:h-screen h-auto md:py-6 py-0">
+    <Section className="lg:flex lg:justify-evenly lg:h-screen h-full md:py-6 py-0">
       <div className="flex lg:flex-row flex-col items-center ">
         <img
           className="lg:pb-16 lg:mr-2"
